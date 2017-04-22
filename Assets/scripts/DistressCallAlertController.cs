ed332fc2b493f988bfe843130b2ecc6302fd2329
Josh Hills <josh.hills@ymail.com>
2017-04-23 00:30:32 +0100

    Folder change to load correct sprite from resources

--- Assets/scripts/DistressCallAlertController.cs
+++ Assets/scripts/DistressCallAlertController.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DistressCallAlertController : MonoBehaviour {
-	private const string PATH_ICON = "Assets/raw/sprites/icons/";
+	private const string PATH_ICON = "sprites/icons/";
 
 	private DistressCallEvent e;
 	private SpriteRenderer icon;
 
-	void Start()
-	{
-		icon = transform.FindChild ("AlertIcon").GetComponent<SpriteRenderer> ();
-	}
-
 	public void init(DistressCallEvent e)
 	{
+		icon = transform.FindChild ("AlertIcon").GetComponent<SpriteRenderer> ();
 		this.e = e;
 		switch(e.getState())
 		{
 			case DistressCallEvent.DistressCallState.IN_PROGRESS:
-				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-progress");
+				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-event-progress");
 				break;
 			case DistressCallEvent.DistressCallState.SUCCEEDED:
-				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-success");
+				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-event-success");
 				break;
 			case DistressCallEvent.DistressCallState.FAILED:
-				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-failure");
+				icon.sprite = Resources.Load<Sprite> (PATH_ICON + "icon-event-failure");
 				break;
 		}
+		Debug.Log (icon.sprite);
 	}
 }
