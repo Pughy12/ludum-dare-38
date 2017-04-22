d0e02346c85b36bc2b871bfd2a9ca5264da6675c
Josh Hills <josh.hills@ymail.com>
2017-04-22 23:09:48 +0100

    Click to transition

--- Assets/scripts/EventLighthouse.cs
+++ Assets/scripts/EventLighthouse.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EventLighthouse : MonoBehaviour {
-	public void transitionToSearching() {
+	void OnMouseDown()
+	{
+		Debug.Log ("hmm");
+		transitionToSearching ();
+	}
+	private void transitionToSearching() {
 		MainController.instance.viewController.getView("ViewSearch").GetComponent<ViewSearchController>().enterView();
 	}
 }
